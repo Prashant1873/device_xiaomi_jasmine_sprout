b99261e79f7633e500c559f465fc803a38ad1b8c
Prashant1873 <shuklaprashant847@gmail.com>
2021-06-10 10:45:23 +0530

    fixes

--- p404_jasmine_sprout.mk
+++ p404_jasmine_sprout.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit ShapeShiftOS product configuration
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/404/config/common_full_phone.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -33,10 +33,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := nad_jasmine_sprout
+PRODUCT_NAME := p404_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
 
