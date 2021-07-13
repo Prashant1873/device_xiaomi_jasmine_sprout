548fb334a71d3aa1d442748c8c9130e2ffcd3b62
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-07-13 14:36:30 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 
 # inherit MI A2 device config for RohieOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_jasmine_sprout.mk
+    $(LOCAL_DIR)/aosp_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_jasmine_sprout-user \
-    cherish_jasmine_sprout-userdebug \
-    cherish_jasmine_sprout-eng
+    aosp_jasmine_sprout-user \
+    aosp_jasmine_sprout-userdebug \
+    aosp_jasmine_sprout-eng
